b926d2430a60816eb1a022dcc42344df768f672c
yuan <yuanqijing00x@gmail.com>
2022-05-05 09:40:22 +0800

    add: context samples

--- context/cmd/basic/main.go
+++ context/cmd/basic/main.go
@@ -8,19 +8,55 @@ import (
 
 func main() {
 	ctx := context.Background()
-	Process(ctx)
+	ctxa, cancela := context.WithCancel(ctx)
+	go a(ctxa)
+	ctxb, cancelb := context.WithCancel(ctxa)
+	go b(ctxb)
+
+	// 先 cancel a 再 cancel b
+	cancela()
+	time.Sleep(time.Second * 3)
+	fmt.Printf("=======")
+	cancelb()
+	/*
+		b canceled
+		a canceled
+		=======
+	*/
+
+	// 先 cancel b 再 cancel a
+	cancelb()
+	time.Sleep(time.Second * 3)
+	fmt.Printf("=======")
+	cancela()
+	time.Sleep(time.Second * 1)
+	/*
+		b canceled
+		=======
+		a canceled
+	*/
 }
 
-func Process(ctx context.Context) {
-	finish := make(chan int)
-	go func() {
-		time.Sleep(3 * time.Second)
-		finish <- 1
-	}()
-	select {
-	case <-ctx.Done():
-		fmt.Println("cancelled")
-	case <-finish:
-		fmt.Println("finished")
+func a(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("a canceled")
+			return
+		default:
+			time.Sleep(time.Second)
+		}
+	}
+}
+
+func b(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("b canceled")
+			return
+		default:
+			time.Sleep(time.Second)
+		}
 	}
 }
